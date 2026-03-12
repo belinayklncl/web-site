3aec42b6e67f74c251c3bfcfbfbb3dc99f554e5b
Belinay Kilincli <belinaykilincli@gmail.com>
2026-03-13 00:53:12 +0200

    new

--- web-app-project/script.js
+++ web-app-project/script.js
@@ -3,3 +3,23 @@ function toggleMenu() {
     document.getElementById("menu").classList.toggle("active");
 
 }
+const images = document.querySelectorAll(".gallery img");
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+
+images.forEach(img => {
+
+    img.addEventListener("click", function () {
+
+        modal.style.display = "flex";
+        modalImg.src = this.src;
+
+    });
+
+});
+
+modal.addEventListener("click", function () {
+
+    modal.style.display = "none";
+
+});
